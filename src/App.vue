8155378ad0c58fba13656868c852cc4e4bb8795f
hamuhamuowo <hemdezvous@gmail.com>
2025-02-25 14:45:18 +0900

    🎉 Set vue project

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  data() {
+    return {
+      msg: 'hello world',
+    }
+  },
+  methods: {
+    onClickAlert() {
+      alert('click!')
+    },
+  },
+}
+</script>
+
+<template>
+  <div>{{ msg }}</div>
+  <button v-on:click="onClickAlert">click</button>
+</template>
+
+<style scoped></style>
